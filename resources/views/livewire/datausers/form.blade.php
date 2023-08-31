4fe4ade11d6a9ab49ac915546396b2f83ffb55a5
Federico Castañeda Ortiz <ffcastaneda@gmail.com>
2023-08-31 03:06:47 -0600

    Datos Complementarios

--- resources/views/livewire/datausers/form.blade.php
+++ resources/views/livewire/datausers/form.blade.php
@@ -1,101 +1,273 @@
-<div class="box box-info padding-1">
-    <div class="box-body">
+<div class="container">
 
-        <div class="form-group">
-            <div class="input-group mb-3">
-                <span class="input-group-text" id="basic-addon1">Nombre Sitio</span>
-                {{ Form::text('website_name', $configuration->website_name,
-                            ['class' => 'form-control' . ($errors->has('website_name') ? ' is-invalid' : ''),
-                            'placeholder' => 'Nombre del sitio'])
-                            }}
-                {!! $errors->first('website_name', '<div class="invalid-feedback">:message</div>') !!}
+    {{-- <x-validation-errors></x-validation-errors> --}}
+
+
+    <div class="row align-items-start">
+        <div class="col-md-4 flex flex-col mt-2">
+            <label class="input-group-text mb-2">Entidad</label>
+            @error('main_record.entidad_id')<div><label for=""></label></div>@enderror
+            <label class="input-group-text mb-2">Municipio</label>
+            @error('main_record.municipio_id')<div><label for=""></label></div>@enderror
+            <label class="input-group-text mb-2">Código Postal</label>
+            @error('main_record.codpos')<div><label for=""></label></div>@enderror
+
+
+            <label class="input-group-text mb-2">Género</label>
+            @error('main_record.gender')<div><label for=""></label></div>@enderror
+            <label class="input-group-text mb-2">F. Nacimiento</label>
+            @error('main_record.birthday')<div><label for=""></label></div>@enderror
+
+            <label class="input-group-text mb-2 mt-1">Curp</label>
+            @error('main_record.curp')<div><label for=""></label></div>@enderror
+        </div>
+
+
+
+        <div class="col flex flex-col mt-2">
+            {{-- Entidad --}}
+            <div class="flex-flex-column mb-2">
+                <select wire:model="main_record.entidad_id"
+                        wire:change="lee_entidad"
+                        {{ $this->allow_edit ? '' : 'disabled'}}
+                        class="form-select form-select-md  sm:mr-2 {{ $errors->has('entidad_id') ? 'bg-red-500' : '' }}"
+                        {{ Auth::user()->has_suplementary_data() ? 'disabled' : ''}}>
+                    <option value="" selected>Elegir</option>
+                    @foreach($entidades as $entidad_select)
+                        <option value="{{ $entidad_select->id }}">{{ $entidad_select->nombre }}</option>
+                    @endforeach
+                </select>
+                @error('main_record.entidad_id')<div class="badge rounded-pill bg-danger">{{ $message }}</div>@enderror
+
+            </div>
+
+            {{-- Municipio --}}
+            <div class="flex-flex-column">
+                <select wire:model="main_record.municipio_id"
+                        {{ $this->allow_edit ? '' : 'disabled'}}
+                        class="form-select form-select-md sm:mr-2 {{ $errors->has('entidad_id') ? 'bg-red-500' : '' }}"
+                        {{ Auth::user()->has_suplementary_data() ? 'disabled' : ''}}
+                        {{ !isset($municipios) ? 'disabled' : ''}}>
+                    <option value="" selected>Elegir</option>
+
+                    @if(isset($municipios) && $municipios->count())
+                        @foreach($entidad->municipios as $municipio)
+                            <option value="{{ $municipio->id }}">{{ $municipio->nombre }}</option>
+                        @endforeach
+                    @endif
+                </select>
+                @error('main_record.municipio_id')<div class="badge rounded-pill bg-danger"></div>@enderror
+
+            </div>
+
+            {{-- Código Postal --}}
+            <div class="flex-flex-column mb-2">
+                <input type="text"
+                    wire:model="main_record.codpos"
+                    {{ $this->allow_edit ? '' : 'disabled'}}
+                    class="block mt-1 rounded {{ $errors->has('codpos') ? 'bg-red-500' : '' }}"
+                    {{ $this->allow_edit ? '' : 'disabled'}}
+                    @if (!$this->allow_edit)
+                        style="background-color:#e9ecef;"
+                    @endif
+                    size="5"
+                    minlength="5"
+                    maxlength="5"
+                >
+                @error('main_record.codpos')<div class="badge rounded-pill bg-danger">{{ $message }}</div>@enderror
+            </div>
+
+            {{--Género --}}
+            <div class="flex-flex-column mb-2">
+                <select wire:model="main_record.gender"
+                        {{ $this->allow_edit ? '' : 'disabled'}}
+                        class="form-select form-select-md sm:mr-2 w-80 {{ $errors->has('gender') ? 'bg-red-500' : '' }}">
+                    <option value="" selected>Elegir</option>
+                    <option value="Hombre" class="bg-blue-500">Hombre</option>
+                    <option value="Mujer" class="bg-pink-500">Mujer</option>
+                </select>
+                @error('main_record.gender')<div class="badge rounded-pill bg-danger">{{ $message }}</div>@enderror
+            </div>
+
+            {{-- Fecha de Nacimiento --}}
+            <div class="flex-flex-column mb-2">
+                    <input wire:model="main_record.birthday"
+                            type="date"
+                            {{ $this->allow_edit ? '' : 'disabled'}}
+                            @if (!$this->allow_edit)
+                                style="background-color:#e9ecef;"
+                            @endif
+                            max="{{Carbon\Carbon::now()->subYear(18)->format('Y-m-d')}}"
+                            class="p-2  border rounded-md w-80 {{ $errors->has('birthday') ? 'bg-red-500' : '' }}"
+                            required
+                    >
+                @error('main_record.birthday')<div class="badge rounded-pill bg-danger">{{ $message }}</div>@enderror
+            </div>
+
+            {{-- CURP --}}
+            <div class="flex-flex-column mb-2">
+                <input wire:model="main_record.curp"
+                        type="text"
+                        {{ $this->allow_edit ? '' : 'disabled'}}
+                        @if (!$this->allow_edit)
+                            style="background-color:#e9ecef;"
+                        @endif
+                        class="p-2 w-full border rounded-md {{ $errors->has('curp') ? 'bg-red-500' : '' }}"
+                        required
+                        maxlength="18"
+                        minlength="18"
+                        pattern="^[A-Z][A,E,I,O,U,X][A-Z]{2}[0-9]{2}[0-1][0-9][0-3][0-9][M,H][A-Z]{2}[B,C,D,F,G,H,J,K,L,M,N,Ñ,P,Q,R,S,T,V,W,X,Y,Z]{3}[0-9,A-Z][0-9]$"
+                >
+                @error('main_record.curp')<div class="badge rounded-pill bg-danger">{{ $message }}</div>@enderror
+                @if ($error_message)
+                    <div class="badge rounded-pill bg-danger">
+                               {{ $error_message }}
+                    </div>
+                @endif
+            </div>
+        </div>
+    </div>
+
+    {{-- Etiquetas para Credencial INE --}}
+    <div class="row align-items-start">
+        <div class="row align-items-start justify-between">
+            <div class="w-1/2 flex flex-col mt-2">
+                <label class="input-group-text mb-2">INE Frontal</label>
+                @error('ine_anverso')<div class="badge rounded-pill bg-danger">{{ $message }}</div>@enderror
+            </div>
+
+            <div class="w-1/2 flex flex-col mt-2">
+                <label class="input-group-text mb-2 text-center">INE Reverso</label>
+                @error('ine_reverso')<div class="badge rounded-pill bg-danger">{{ $message }}</div>@enderror
+
+            </div>
+        </div>
+    </div>
+
+    @if($this->allow_edit)
+        {{-- Seleccionar Archivos de INE --}}
+        <div class="row align-items-start">
+            <div class="row align-items-start justify-between">
+                <div class="w-1/2 flex flex-col mt-2">
+                    <div class="col-lg-6  col-md-8 mb-4 {{ $errors->has('curp') ? 'bg-red-500' : '' }}">
+                        <input type="file"
+                                wire:model="ine_anverso"
+                                {{-- wire:loading wire:target="store" --}}
+                                {{$allow_edit ? '' : 'disabled' }}
+                                class="form-control">
+                    </div>
+                </div>
+
+                <div class="w-1/2 flex flex-col mt-2">
+                    <div class="col-lg-6  col-md-8 mb-4">
+                        <input type="file"
+                                wire:model="ine_reverso"
+                                {{-- wire:loading wire:target="store" --}}
+                                {{$allow_edit ? '' : 'disabled' }}
+                                class="form-control">
+                    </div>
+                </div>
             </div>
         </div>
 
-        <div class="input-group mb-3">
-            <span class="input-group-text" id="basic-addon1">Url</span>
-            {{ Form::text('website_url', $configuration->website_url,
-            ['size' => '80',
-                'class' =>  ($errors->has('website_url') ? ' is-invalid' : ''), 'placeholder' => 'Url']) }}
-            {!! $errors->first('website_url', '<div class="invalid-feedback">:message</div>') !!}
-        </div>
-        <div class="input-group mb-3">
-            <span class="input-group-text" id="basic-addon1">Correo</span>
-            {{ Form::text('email', $configuration->email, ['class' => 'form-control' . ($errors->has('email') ? ' is-invalid' : ''), 'placeholder' => 'Email']) }}
-            {!! $errors->first('email', '<div class="invalid-feedback">:message</div>') !!}
-        </div>
+        {{-- Imagen Temporal --}}
+        <div class="row align-items-start">
+            <div class="row align-items-start justify-between">
+                <div class="w-1/2 flex flex-col mt-2">
+                    @if (isset($ine_anverso))
+                        @if(in_array($this->ine_anverso->getClientOriginalExtension(),$extensions_file))
+                            <img src="{{ $ine_anverso->temporaryUrl() }}" class="avatar-md">
+                        @else
+                            <div class="flex items-center">
+                                    <label for="" class="underline text-sm text-red-600 dark:text-gray-400 hover:text-red-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 dark:focus:ring-offset-gray-800">
+                                        Formato no permitido: Debe ser imagen
+                                    </label>
+                            </div>
+                        @endif
+                    @endif
 
+                </div>
 
-        <div class="form-group">
-            <div class="input-group mb-3">
-                <span class="input-group-text" id="basic-addon1">Minutos Antes para pronóstico</span>
-                <input type="number"
-                        name="minuts_before_picks"
-                        id="minuts_before_picks"
-                        max="30"
-                        min="5"
-                        size="5"
-                        value="{{  $configuration->minuts_before_picks }}"
-                        class="{{ $errors->has('minuts_before_picks') ? ' is-invalid' : '' }}"
-                >
-              </div>
-              {!! $errors->first('minuts_before_picks', '<div class="invalid-feedback">:message</div>') !!}
-        </div>
+                <div class="w-1/2 flex flex-col mt-2">
+                    <div class="col-lg-6  col-md-8 mb-4">
+                        @if (isset($ine_reverso))
+                            @if(in_array($this->ine_reverso->getClientOriginalExtension(),$extensions_file))
+                                <img src="{{ $ine_reverso->temporaryUrl() }}" class="avatar-md">
+                            @else
+                                <div class="flex items-center">
+                                        <label for="" class="underline text-sm text-red-600 dark:text-gray-400 hover:text-red-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 dark:focus:ring-offset-gray-800">
+                                            Formato no permitido: Debe ser imagen
+                                        </label>
+                                </div>
+                            @endif
+                        @endif
 
-        {{-- ¿Permite Empates? --}}
-        <div class="input-group mb-3">
-            <span class="input-group-text" id="basic-addon1">¿Permite Empate?</span>
-                <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
-                    <div class="ml-3">
-                        <label class="radio-inline">
-                            <input type="radio" name="allow_tie" value="1" {{ $configuration->allow_tie ? 'checked' : ''}}/> SI
-                        </label>
-                    </div>
-                    <div class="ml-3">
-                        <label class="radio-inline">
-                            <input type="radio" name="allow_tie" value="0" {{ !$configuration->allow_tie ? 'checked' : ''}}/> NO
-                        </label>
                     </div>
                 </div>
+            </div>
         </div>
 
-        {{-- ¿Solicitar Resultaos en Pronóstico? --}}
-
-        <div class="input-group mb-3">
-            <span class="input-group-text" id="basic-addon1">¿Solicitar Puntos en Pronósticos?</span>
-                <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
-                    <div class="ml-3">
-                        <label class="radio-inline">
-                            <input type="radio" name="score_picks" value="1" {{ $configuration->score_picks ? 'checked' : ''}}/> SI
-                        </label>
+        {{-- Aceptar que se grabaran --}}
+        <div class="row align-items-start">
+            <div class="mt-4">
+                <x-label for="confirmar">
+                    <div class="flex items-center">
+                        <x-checkbox wire:model="confirmar" required />
+                        <div class="ml-2">
+                            <label for="" class="underline text-sm text-red-600 dark:text-gray-400 hover:text-red-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 dark:focus:ring-offset-gray-800">
+                                Acepto que al enviar mis datos no podrán ser modificados
+                            </label>
+                        </div>
                     </div>
-                    <div class="ml-3">
-                        <label class="radio-inline">
-                            <input type="radio" name="score_picks" value="0" {{ !$configuration->score_picks ? 'checked' : ''}}/> NO
-                        </label>
+                </x-label>
+                @error('confirmar')<span class="badge rounded-pill bg-danger">{{ $message }}</span>@enderror
+            </div>
+        </div>
+@else
+        {{-- Imagenes Grabadas --}}
+        <div class="row align-items-start">
+            <div class="row align-items-start justify-between">
+                <div class="w-1/2 flex flex-col mt-2 justify-center">
+                      @if (isset($main_record->ine_anverso))
+                        <img src="{{Storage::url($main_record->ine_anverso)}}" class="avatar-sm" alt="">
+                    @endif
+                </div>
+
+                <div class="w-1/2 flex flex-col mt-2 justify-center">
+                    <div class="col-lg-6  col-md-8 mb-4">
+                        @if (isset($main_record->ine_reverso))
+                            <img src="{{Storage::url($main_record->ine_reverso)}}" class="avatar-sm" alt="">
+                        @endif
                     </div>
                 </div>
+            </div>
         </div>
+@endif
 
-        {{-- ¿Generar pronósticos faltantes? --}}
 
-        <div class="input-group mb-3">
-            <span class="input-group-text" id="basic-addon1">¿Solicitar Puntos en Pronósticos?</span>
-                <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
-                    <div class="ml-3">
-                        <label class="radio-inline">
-                            <input type="radio" name="create_mssing_picks" value="1" {{ $configuration->create_mssing_picks ? 'checked' : ''}}/> SI
-                        </label>
-                    </div>
-                    <div class="ml-3">
-                        <label class="radio-inline">
-                            <input type="radio" name="create_mssing_picks" value="0" {{ !$configuration->create_mssing_picks ? 'checked' : ''}}/> NO
-                        </label>
-                    </div>
+
+    {{-- Botones de acción --}}
+    <div class="modal-footer">
+
+        <div class="d-flex justify-content-around">
+            @if(!$this->allow_edit)
+                <div class="w-1/2">
+                    <p class="text-white">Si necesita actualizar los datos póngase en contacto con un administrador</p>
                 </div>
+            @endif
+            <button wire:click="closeModal()"
+                class="btn btn-danger">
+                {{__("Cancel")}}
+            </button>
+
+            @if($this->allow_edit)
+                <button  onclick="confirm_user_data()"
+                    class="btn btn-success"
+                    title="Guardar">
+                    {{__("Save")}}
+                    <i class="mdi mdi-content-save-alert"></i>
+                </button>
+            @endif
         </div>
-    </div>
-    <div class="box-footer mt20 float-right">
-        <button type="submit" class="btn btn-primary">{{ __('Submit') }}</button>
     </div>
 </div>
