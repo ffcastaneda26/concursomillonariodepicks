3395ab6b63b6f7f59252e07f10dbd9c63dc005e3
Federico Castañeda Ortiz <ffcastaneda@gmail.com>
2023-09-09 10:22:53 -0600

    Ajustar los iconos en tabla de pronósticos (Falta ordenar detalle de los pronósticos con los partidos del encabezado)

--- resources/views/livewire/picks-round-user.blade.php
+++ resources/views/livewire/picks-round-user.blade.php
@@ -1,17 +1,15 @@
 <div class="mt-2">
     <td>{{ $user->name }}</td>
     @foreach($user_picks_round as $user_pick_round)
+
         <td align="center">
             @if($user_pick_round->game->allow_pick())
                 <img src="{{ asset('images/reloj.jpg') }}" alt="" width="32px" height="32px">
             @else
                 @if($user_pick_round->winner== 1)
-                    <label class="text-sm">{{ $user_pick_round->game->local_team->name}}</label>
-
-                    {{-- <label class="{{ $user_pick_round->winner== $user_pick_round->game->winner ? 'bg-danger' : 'bg-success' }}">{{ $user_pick_round->game->local_team->name}}</label> --}}
+                    <img src="{{Storage::url($user_pick_round->game->local_team->logo)}}"  class="avatar-xs" alt="" width="25px" height="25px">
                 @else
-                    <label class="text-sm">{{ $user_pick_round->game->visit_team->name}}</label>
-                    {{-- <label class="{{ $user_pick_round->winner== $user_pick_round->game->winner ? 'bg-danger' : 'bg-success' }}">{{ $user_pick_round->game->visit_team->name}}</label> --}}
+                    <img src="{{Storage::url($user_pick_round->game->visit_team->logo)}}"  class="avatar-xs" alt="" width="25px" height="25px">
                 @endif
                 <span>
                     <img src="{{ $user_pick_round->winner== $user_pick_round->game->winner  ?   asset('images/afirmativo.png') : asset('images/negativo.png')}}"
@@ -19,7 +17,9 @@
                                 height="12px"
                                 width="12px">
                 </span>
-
+                @if($user_pick_round->selected)
+                    <span class="bg-dark text-white text-center text-sm">SEL</span>
+                @endif
             @endif
 
         </td>
--- resources/views/livewire/results/header_game_teams.blade.php
+++ resources/views/livewire/results/header_game_teams.blade.php
@@ -3,7 +3,22 @@
         {{-- <th>Id</th> --}}
         <th>Participante</th>
         @foreach ($round_games as $game)
-            <td>{{ $game->visit_team->short }} - {{ $game->local_team->short }}</td>
+            <td><label  class="{{ $game->winner == 2 ? 'font-bold text-base' : '' }}"> {{ $game->visit_team->short }}</label>
+                -
+                <label  class="{{ $game->winner == 1 ? 'font-bold text-base' : '' }}"> {{ $game->local_team->short }}</label>
+            </td>
         @endforeach
+    </tr>
+    <tr class="bg-dark text-white text-center text-sm">
+        <td></td>
+        @foreach ($round_games as $game)
+        <td>
+            @if($game->visit_points || $game->local_pints)
+                {{$game->visit_points ? $game->visit_points : '0' }}
+                -
+                {{ $game->local_points ? $game->local_points : '0' }}
+            @endif
+        </td>
+    @endforeach
     </tr>
 </thead>
