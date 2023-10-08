aa1f613c60afc8bcb1e357886f9e506774d5a1ab
Federico Castañeda Ortiz <ffcastaneda@gmail.com>
2023-10-08 01:53:42 -0600

    Tabla de pronósticos se pone nombre corto cuando no se seleccionó y el icono cuando si

--- resources/views/livewire/results/user-pick-game.blade.php
+++ resources/views/livewire/results/user-pick-game.blade.php
@@ -3,24 +3,36 @@
         @if($allow_pick)
             <img src="{{ asset('images/reloj.jpg') }}" alt="" width="32px" height="32px">
         @else
-            @if($user_pick_round->winner== 1)
-                <img src="{{Storage::url($user_pick_round->game->local_team->logo)}}"  class="avatar-xs">
-            @else
-                <img src="{{Storage::url($user_pick_round->game->visit_team->logo)}}"  class="avatar-xs">
-            @endif
-
             @if($has_result)
                 <span>
-                    <img src="{{ $hit_game  ? asset('images/afirmativo.png') : asset('images/negativo.png')}}"
-                                height="10px"
-                                width="10px"
+                    <img src="{{ $hit_game  ? asset('images/afirmativo.png')
+                                            : asset('images/negativo.png')}}"
+                                height="16px"
+                                width="16px"
                         >
                 </span>
+                <br>
             @endif
 
             @if($game_selected)
-                 <label class="bg-secondary text-white text-center">SEL</label>
+                @if($user_pick_round->winner== 1)
+                    <img src="{{Storage::url($user_pick_round->game->local_team->logo)}}"  class="avatar-xs">
+                @else
+                    <img src="{{Storage::url($user_pick_round->game->visit_team->logo)}}"  class="avatar-xs">
+                @endif
+            @else
+                @if($user_pick_round->winner== 1)
+                    <label class="text-black text-center">{{ $user_pick_round->game->local_team->short }}</label>
+                @else
+                    <label class="text-black text-center">{{ $user_pick_round->game->visit_team->short }}</label>
+                @endif
             @endif
+
+
+
+            {{-- @if($game_selected)
+                 <label class="bg-secondary text-white text-center">SEL</label>
+            @endif --}}
         @endif
     </td>
 </div>
