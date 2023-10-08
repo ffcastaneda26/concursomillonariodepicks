07d3bc03e29d570f599f092c2fb73c08b9147b53
Federico Castañeda Ortiz <ffcastaneda@gmail.com>
2023-10-08 01:33:42 -0600

    Tabla de pronósticos se puso en CARD

--- resources/views/livewire/results/index.blade.php
+++ resources/views/livewire/results/index.blade.php
@@ -3,27 +3,31 @@
     <div class="container-fluid mt-2">
         @if(isset($round_games) && !empty($round_games))
             <div class="flex flex-row justify-center">
-                <div class="col-sm-12">
-                    <div class="table-responsive">
-                        <table class="table text-xs">
-                            @include('livewire.results.header_game_teams')
-                            <tbody>
-                                @foreach ($records as $pick_user)
-                                    <tr>
-                                        <td>{{ $pick_user->name }}</td>
-                                        @foreach($selected_round->picks_user($pick_user->id)->get() as $pick)
-                                            @livewire('user-pick-game', ['user'=> $pick->user_id,'game'=> $pick->game_id], key($pick->id))
-                                        @endforeach
-                                        <td class="text-base text-center">
-                                            {{ $pick_user->has_position_record_round($selected_round->id) ? $pick_user->hits_round($selected_round->id) : ''}}
-                                        </td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                        @include('common.crud_pagination')
+                    <div class="card">
+                        <div class="card-body ">
+                            <div class="col-sm-12">
+                                <div class="table-responsive">
+                                    <table class="table text-xs">
+                                        @include('livewire.results.header_game_teams')
+                                        <tbody>
+                                            @foreach ($records as $pick_user)
+                                                <tr>
+                                                    <td>{{ $pick_user->name }}</td>
+                                                    @foreach($selected_round->picks_user($pick_user->id)->get() as $pick)
+                                                        @livewire('user-pick-game', ['user'=> $pick->user_id,'game'=> $pick->game_id], key($pick->id))
+                                                    @endforeach
+                                                    <td class="text-base text-center">
+                                                        {{ $pick_user->has_position_record_round($selected_round->id) ? $pick_user->hits_round($selected_round->id) : ''}}
+                                                    </td>
+                                                </tr>
+                                            @endforeach
+                                        </tbody>
+                                    </table>
+                                    @include('common.crud_pagination')
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                </div>
             </div>
         @endif
     </div>
