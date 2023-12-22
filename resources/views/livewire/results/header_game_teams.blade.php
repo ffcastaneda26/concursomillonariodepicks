8ac8e070cd98cd0b7460b6c6a63bc4d516f03a2c
Federico Castañeda Ortiz <ffcastaneda@gmail.com>
2023-12-22 14:44:58 -0600

    Tabla de pronósticos encabezado de partidos coloca marcador siempre y cuando tanto puntos de visita y local NO SEAN NULL

--- resources/views/livewire/results/header_game_teams.blade.php
+++ resources/views/livewire/results/header_game_teams.blade.php
@@ -3,9 +3,9 @@
         <th rowspan="2" valign="middle">PARTICIPANTE</th>
         @foreach ($round_games as $game)
             <td class="text-left">
-                <img src="{{Storage::url($game->visit_team->logo)}}" class="avatar-xs">
+                <img src="{{ Storage::url($game->visit_team->logo) }}" class="avatar-xs">
                 <br>
-                <img src="{{Storage::url($game->local_team->logo)}}" class="avatar-xs">
+                <img src="{{ Storage::url($game->local_team->logo) }}" class="avatar-xs">
             </td>
         @endforeach
         <th valign="middle" colspan="2"></th>
@@ -13,14 +13,15 @@
     <tr class="bg-dark text-white text-center text-sm">
         @foreach ($round_games as $game)
             <td>
-                @if($game->visit_points || $game->local_pints)
-                    {{$game->visit_points ? $game->visit_points : '0' }}
+                @if (!is_null($game->visit_points) && is_null($game->local_pints))
+                    {{ $game->visit_points ? $game->visit_points : '0' }}
                     -
                     {{ $game->local_points ? $game->local_points : '0' }}
                 @endif
             </td>
         @endforeach
-         <th>MNF</th>
+        <th>MNF</th>
         <th valign="middle">ACIERTOS</th>
     </tr>
 </thead>
+
